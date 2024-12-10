ed609b81e774735ed80f78f8c8a41ddb5ec53c5a
Yuan Tang <yuantang@g.harvard.edu>
2024-12-10 18:15:48 -0500

    yuan updated ml workflow

---
+++ src/workflow/cli.py
@@ -0,0 +1,217 @@
+"""
+Module that contains the command line app.
+
+Typical usage example from command line:
+        python cli.py
+"""
+
+import os
+import argparse
+import random
+import string
+from kfp import dsl
+from kfp import compiler
+import google.cloud.aiplatform as aip
+
+
+GCP_PROJECT = os.environ["GCP_PROJECT"]
+GCS_BUCKET_NAME = os.environ["GCS_BUCKET_NAME"]
+BUCKET_URI = f"gs://{GCS_BUCKET_NAME}"
+PIPELINE_ROOT = f"{BUCKET_URI}/pipeline_root/root"
+GCS_SERVICE_ACCOUNT = os.environ["GCS_SERVICE_ACCOUNT"]
+GCP_REGION = os.environ["GCP_REGION"]
+
+DATA_CREATOR_IMAGE = "hiyt/tripee-data-creator"
+GEMINI_FINETUNER_IMAGE = "hiyt/tripee-gemini-finetuner"
+
+def generate_uuid(length: int = 8) -> str:
+    return "".join(random.choices(string.ascii_lowercase + string.digits, k=length))
+
+def data_creator():
+    print("data_creator()")
+
+    # Define a Container Component
+    @dsl.container_component
+    def data_creator():
+        container_spec = dsl.ContainerSpec(
+            image=DATA_CREATOR_IMAGE,
+            command=[],
+            args=[
+                "cli.py",
+                "--generate",
+                "--prepare",
+                "--save_prompt",
+                f"--upload",
+            ],
+        )
+        return container_spec
+
+    # Define a Pipeline
+    @dsl.pipeline
+    def data_creator_pipeline():
+        data_creator()
+
+    # Build yaml file for pipeline
+    compiler.Compiler().compile(
+        data_creator_pipeline, package_path="data_creator.yaml"
+    )
+
+    # Submit job to Vertex AI
+    aip.init(project=GCP_PROJECT, staging_bucket=BUCKET_URI)
+
+    job_id = generate_uuid()
+    DISPLAY_NAME = "tripee-data-creator-" + job_id
+    job = aip.PipelineJob(
+        display_name=DISPLAY_NAME,
+        template_path="data_creator.yaml",
+        pipeline_root=PIPELINE_ROOT,
+        enable_caching=False,
+    )
+
+    job.run(service_account=GCS_SERVICE_ACCOUNT)
+
+
+def gemini_finetuner():
+    print("gemini_finetuner()")
+
+    # Define a Container Component for gemini_finetuner
+    @dsl.container_component
+    def gemini_finetuner():
+        container_spec = dsl.ContainerSpec(
+            image=GEMINI_FINETUNER_IMAGE,
+            command=[],
+            args=[
+                "cli.py",
+                "--train",
+            ],
+        )
+        return container_spec
+
+    # Define a Pipeline
+    @dsl.pipeline
+    def gemini_finetuner_pipeline():
+        gemini_finetuner()
+
+    # Build yaml file for pipeline
+    compiler.Compiler().compile(
+        gemini_finetuner_pipeline, package_path="gemini_finetuner.yaml"
+    )
+
+    # Submit job to Vertex AI
+    aip.init(project=GCP_PROJECT, staging_bucket=BUCKET_URI)
+
+    job_id = generate_uuid()
+    DISPLAY_NAME = "tripee-gemini_finetuner-" + job_id
+    job = aip.PipelineJob(
+        display_name=DISPLAY_NAME,
+        template_path="gemini_finetuner.yaml",
+        pipeline_root=PIPELINE_ROOT,
+        enable_caching=False,
+    )
+
+    job.run(service_account=GCS_SERVICE_ACCOUNT)
+
+
+def pipeline():
+    print("pipeline()")
+    # Define a Container Component for data creator
+    @dsl.container_component
+    def data_creator():
+        container_spec = dsl.ContainerSpec(
+            image=DATA_CREATOR_IMAGE,
+            command=[],
+            args=[
+                "cli.py",
+                "--generate",
+                "--prepare",
+                "--save_prompt",
+                f"--upload",
+            ],
+        )
+        return container_spec
+
+    # Define a Container Component for gemini finetuner
+    @dsl.container_component
+    def gemini_finetuner():
+        container_spec = dsl.ContainerSpec(
+            image=GEMINI_FINETUNER_IMAGE,
+            command=[],
+            args=[
+                "cli.py",
+                "--train",
+            ],
+        )
+        return container_spec
+
+    # Define a Pipeline
+    @dsl.pipeline
+    def ml_pipeline():
+        # Data Creator
+        data_creator_task = (
+            data_creator()
+            .set_display_name("Data Creator")
+            .set_cpu_limit("500m")
+            .set_memory_limit("2G")
+        )
+        # Gemini Finetuner
+        gemini_finetuner_task = (
+            gemini_finetuner()
+            .set_display_name("Gemini Finetuner")
+            .after(data_creator_task)
+        )
+
+    # Build yaml file for pipeline
+    compiler.Compiler().compile(ml_pipeline, package_path="pipeline.yaml")
+
+    # Submit job to Vertex AI
+    aip.init(project=GCP_PROJECT, staging_bucket=BUCKET_URI)
+
+    job_id = generate_uuid()
+    DISPLAY_NAME = "tripee-pipeline-" + job_id
+    job = aip.PipelineJob(
+        display_name=DISPLAY_NAME,
+        template_path="pipeline.yaml",
+        pipeline_root=PIPELINE_ROOT,
+        enable_caching=False,
+    )
+
+    job.run(service_account=GCS_SERVICE_ACCOUNT)
+
+
+def main(args=None):
+    print("CLI Arguments:", args)
+
+    if args.data_creator:
+        data_creator()
+
+    if args.gemini_finetuner:
+        print("Gemini Finetuner")
+        gemini_finetuner()
+
+    if args.pipeline:
+        pipeline()
+
+if __name__ == "__main__":
+    # Generate the inputs arguments parser
+    # if you type into the terminal 'python cli.py --help', it will provide the description
+    parser = argparse.ArgumentParser(description="Workflow CLI")
+
+    parser.add_argument(
+        "--data_creator",
+        action="store_true",
+        help="Run just the Data Creator",
+    )
+    parser.add_argument(
+        "--gemini_finetuner",
+        action="store_true",
+        help="Run just the Gemini Finetuner",
+    )
+    parser.add_argument(
+        "--pipeline",
+        action="store_true",
+        help="Cheese App Pipeline",
+    )
+
+    args = parser.parse_args()
+
+    main(args)
---
+++ src/workflow/docker-shell.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# set -e
+
+export IMAGE_NAME="tripee-workflow"
+export BASE_DIR=$(pwd)
+export SECRETS_DIR=$(pwd)/../../secrets/
+export GCP_PROJECT="ac215-3h2b-yuantang"
+export GCS_BUCKET_NAME="llm-strict-format-dataset"
+export GCS_SERVICE_ACCOUNT="llm-service-account@ac215-3h2b-yuantang.iam.gserviceaccount.com"
+export GCP_REGION="us-central1"
+
+# Build the image based on the Dockerfile
+docker build -t $IMAGE_NAME --platform=linux/amd64 -f Dockerfile .
+
+# Run Container
+docker run --rm --name $IMAGE_NAME -ti \
+-v /var/run/docker.sock:/var/run/docker.sock \
+-v "$BASE_DIR":/app \
+-v "$SECRETS_DIR":/secrets \
+-v "$BASE_DIR/../data-creator":/data-creator \
+-v "$BASE_DIR/../gemini-finetuner":/gemini-finetuner\
+-e GOOGLE_APPLICATION_CREDENTIALS=/secrets/llm-service-account-key.json \
+-e GCP_PROJECT=$GCP_PROJECT \
+-e GCS_BUCKET_NAME=$GCS_BUCKET_NAME \
+-e GCS_SERVICE_ACCOUNT=$GCS_SERVICE_ACCOUNT \
+-e GCP_REGION=$GCP_REGION \
+$IMAGE_NAME
+
